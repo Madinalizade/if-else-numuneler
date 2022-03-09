d292296451a8b87162a0ba0056a340ed97e2e48b
madinaalizada@outlook.com <madinaalizada@outlook.com>
2022-03-09 17:52:01 +0200

    Add project files.

---
+++ if else numuneler/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace if_else_numuneler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //6
+            /* double x = double.Parse(Console.ReadLine());
+              if (x<=7)
+                  Console.WriteLine(3*x-9);
+              else 
+                  Console.WriteLine(1/(Math.Pow(x,2)-4));*/
+            //7
+            /*double x = double.Parse(Console.ReadLine());
+            if (x>=0 && x<=3)
+                Console.WriteLine(Math.Pow(x,2));
+            else
+                Console.WriteLine("4");*/
+            //8
+            /* double x = double.Parse(Console.ReadLine());
+             if(x<=2)
+                 Console.WriteLine(Math.Pow(x,2)+4*x+5);
+             else
+                 Console.WriteLine(1/(Math.Pow(x,2)+4*x+5));*/
+            //9
+            /* double x = double.Parse(Console.ReadLine());
+             if (x>=0 && x<=1)
+                 Console.WriteLine(Math.Pow(x,2)-x);
+             else
+                 Console.WriteLine(Math.Pow(x,2)-Math.Sin(180*Math.Pow(x,2)));*/
+            //10
+            /*double x = double.Parse(Console.ReadLine());
+            if(x>=8)
+                Console.WriteLine(-Math.Pow(x,2)+x-9);
+            else
+                Console.WriteLine(1/(Math.Pow(x,4)-6));*/
+            //11
+            /*double x = double.Parse(Console.ReadLine());
+            if(x>=-3.5)
+                Console.WriteLine(4*Math.Pow(x,2)+2*x-19);
+            else if(x<3.5)
+                Console.WriteLine((-2*x)/(4*x+1));*/
+        }
+    }
+}
